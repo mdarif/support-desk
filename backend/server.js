3eaaf43e390bf2916e929454062d26e002cc48fe
Mohammad Arif <arif_iq@yahoo.co.in>
2022-02-27 19:58:01 +0530

    Controller & Middleware introduces along-with routes, 'express-async-handler' for handling exception inside of async express routes.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Server is running...')
+const express = require('express') // commonjs module syntax
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' })
+})
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
