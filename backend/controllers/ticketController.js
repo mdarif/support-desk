3b9ff5372ac230dea450aea413b22c20637b3496
Mohammad Arif <arif_iq@yahoo.co.in>
2022-03-04 06:58:19 +0530

    Get/create tickets controller methods are in place.

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,46 @@ const Ticket = require('../models/ticketModel')
 // @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get tickets' })
+  // Get user using the id and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const tickets = await Ticket.find({ user: req.user._id })
+
+  res.status(200).json(tickets)
 })
 
 // @desc    Create new ticket
 // @route   POST /api/ticket
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Create tickets' })
+  const { product, description } = req.body
+
+  if (!product || !description) {
+    res.status(400) // Bad request
+    throw new Error('Please provide a product and description')
+  }
+
+  // Get user using the id and JWT
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new'
+  })
+
+  res.status(201).json(ticket)
 })
 
 module.exports = {
