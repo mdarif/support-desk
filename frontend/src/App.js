d1ee24e41fb13d1e22fc34474892856445d4ee5c
Mohammad Arif <arif_iq@yahoo.co.in>
2022-03-04 14:53:49 +0530

    Private route component, useAuthStatus.js hooks, new ticket screen are in place.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header'
+import { PrivateRoute } from './components/PrivateRoute'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import NewTicket from './pages/NewTicket'
 import Register from './pages/Register'
 
 function App () {
@@ -16,6 +18,9 @@ function App () {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,5 @@
+function NewTicket() {
+  return <h1>New Ticket</h1>;
+}
+
+export default NewTicket;
