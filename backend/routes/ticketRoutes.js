04a8639b1397b8f39797567871100ca6c883e504
Mohammad Arif <arif_iq@yahoo.co.in>
2022-03-07 09:09:25 +0530

    Notes Backend is ready which means we have created Note Controller, Model, Routes & also fixed the small issue around Node Server while placing --ignore client in package.json

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -10,6 +10,10 @@ const {
 
 const { protect } = require('../middleware/authMiddleware')
 
+// Re-route into note router
+const noteRouter = require('./noteRoutes')
+router.use('/:ticketId/notes', noteRouter)
+
 // Protected route to create the ticket
 router
   .route('/')
